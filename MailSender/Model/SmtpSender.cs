e29be46bdc04ff0d8f8d0065240a7b945f8aab6d
Pirozhok1985 <pirozhok1985@gmail.com>
2021-03-09 20:48:00 +1000

    Добавил view model

--- MailSender/Model/SmtpSender.cs
+++ MailSender/Model/SmtpSender.cs
@@ -13,18 +13,26 @@ namespace MailSender.Model
         public SmtpClient Client { get; set; }
         public string Subject { get; set; }
         public string Body { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
 
 
         public SmtpSender()
         {
-            Client = new SmtpClient("smtp.gmail.com", 587);
+            Client = new SmtpClient();
+            Client.Host = "smtp.gmail.com";
+            Client.Port = 587;
             Client.Credentials = new NetworkCredential("pirozhok1985", "xxmbemcbbyffhbps");
             Client.EnableSsl = true;
+            From = "pirozhok1985@gmail.com";
+            To = "vic1985@inbox.ru";
+            Subject = "test subject";
+            Body = "Hello! World!";
         }
 
-        public void Send(string from, string to, string subject, string body)
+        public void Send()
         {
-            this.Client.Send("pirozhok1985@gmail.com", "vic1985@inbox.ru", "test subject", "Hello! World!");
+            this.Client.Send(From, To, Subject, Body);
         }
     }
 }
