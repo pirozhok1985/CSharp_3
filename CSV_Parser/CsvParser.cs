2bce9fd51379ffd6bad78f31072d7b62c6c6d240
pirozhok <pirozhok1985@gmail.com>
2021-03-29 10:46:07 +1000

    Создал класс CscvParser и релизовал метод LoadContent

---
+++ CSV_Parser/CsvParser.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CSV_Parser
+{
+   static class CsvParser
+    {
+        private static Dictionary<string, List<string>> parcedString = new Dictionary<string, List<string>>();
+        public static char Delimeter { get; set; }
+        public static void SaveContent(string filename)
+        { 
+        }
+        public static void LoadContent(string filename)
+        {
+            using (StreamReader csvReader = new StreamReader(filename))
+            {
+                string[] csvHeader = csvReader.ReadLine().Split(Delimeter);
+                string csvString = string.Empty;
+                while (!csvReader.EndOfStream)
+                {
+                    csvString = csvReader.ReadLine();
+                    for (var i = 0; i < csvHeader.Length; i++) 
+                    {
+                        if (parcedString.ContainsKey(csvHeader[i])) 
+                        {
+                            parcedString[csvHeader[i]].Add(csvString.Split(Delimeter)[i]);
+                        }
+                        else
+                        {
+                            parcedString.Add(csvHeader[i], new List<string>());
+                            parcedString[csvHeader[i]].Add(csvString.Split(Delimeter)[i]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ CSV_Parser/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CSV_Parser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string csvFile = @"c:\users\Pirozhok\Downloads\catalog.csv";
+            CsvParser.Delimeter = ';';
+            CsvParser.LoadContent(csvFile);
+        }
+    }
+}
